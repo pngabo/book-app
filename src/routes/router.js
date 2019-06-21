ff238636ba3f5abdc9667957fff472e464240552
Patrick Ngabonziza <ngabonzizap7@gmail.com>
2019-06-21 08:39:54 +0200

    [ft] delete-book

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,6 +8,6 @@ router.get('/api/v1/books', BookController.getAllBooks);
 router.post('/api/v1/books', BookController.addBook);
 router.get('/api/v1/books/:id', BookController.getABook);
 router.put('/api/v1/books/:id', BookController.updatedBook);
-// router.delete('/api/v1/books/:id', BookController.deleteBook);
+router.delete('/api/v1/books/:id', BookController.deleteBook);
 
 export default router;
