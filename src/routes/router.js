4cfa2f6bf6c46954cc0998e76f7607c8c9587973
Patrick Ngabonziza <ngabonzizap7@gmail.com>
2019-09-26 12:44:37 +0200

    fix tests

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/api/v1/books', BookController.getAllBooks);
 router.post('/api/v1/books', BookController.addBook);
 router.get('/api/v1/books/:id', BookController.getABook);
-router.put('/api/v1/books/:id', BookController.updatedBook);
+router.patch('/api/v1/books/:id', BookController.updatedBook);
 router.delete('/api/v1/books/:id', BookController.deleteBook);
 
 export default router;
